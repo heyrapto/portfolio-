95ddd2e259188dd315888dc473c275e385d37f79
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-17 13:24:06 +0100

    background lines

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,12 @@ import SvgIcons from "../assets/icons/SvgIcons";
 const Hero = () => {
     return (
       <header className="my-[5.4rem] flex items-center justify-center min-h-screen relative overflow-hidden w-full lg:min-h-[100vh] md:mt-0">
-        <div className="my_fixed_width md:flex justify-center items-center sm:pr-2 md:px-3">
+        <div className="my_fixed_width md:flex justify-center items-center sm:pr-2 md:px-3 text-center">
           <div className="md:w-[52%] lg:w-[60%]">
             <h1 className="gradient_text text-[2.2rem] font-clash leading-[2.5rem] md:leading-[3.6rem] md:text-[3.5rem]">
               Creative <br className="md:hidden" /> Software Developer.
             </h1>
-            <div className="font-[400] mt-[2.5rem] font-rubik pr-5 text-sm">
+            <div className="font-[400] mt-[2.5rem] font-rubik pr-5 text-sm text-center">
               <p className="gradient_text">
                 You got here! I'm <span className="bg-transparent bg-clip-text bg-gradient from-violet-400 to-violet-700">Rapto</span>, a Fullstack Developer heavy on Frontend and passionate about Web3, Blockchain and writing smart contracts. 
               </p>
@@ -16,7 +16,7 @@ const Hero = () => {
                 I develop industry standard products that solves everyday problems with an emphasis on user experience, accessibility and performance.
               </p>
             </div>
-            <div className="mt-[1.7rem] flex space-x-5">
+            <div className="mt-[1.7rem] flex space-x-5 items-center justify-center">
               <a
                 href="https://www.facebook.com/raptomiofw"
                 className="rounded-full border-2 border-[#34afcb] hover:border-green hover:bg-green md:p-2 p-0"
