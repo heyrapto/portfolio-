0abb50df0257f7c82ccaf29361ad0f825024d7ef
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-02-05 05:02:21 -0800

    reduced image widths

--- src/components/ui/animated-testimonials.tsx
+++ src/components/ui/animated-testimonials.tsx
@@ -42,7 +42,7 @@ export const AnimatedTestimonials = ({
   };
   return (
     <div className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20 z-0">
-      <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20">
+      <div className="relative grid grid-cols-1 md:grid-cols-2 gap-20">
         <div>
           <div className="relative h-80 w-full">
             <AnimatePresence>
@@ -83,7 +83,7 @@ export const AnimatedTestimonials = ({
                     width={500}
                     height={500}
                     draggable={false}
-                    className="h-full w-full rounded-3xl object-cover object-center md:w-[300px] w-250px]"
+                    className="h-full w-full rounded-3xl object-cover object-center md:w-[500px] w-[300px]"
                   />
                 </motion.div>
               ))}
