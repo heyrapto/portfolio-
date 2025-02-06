d23a73b8be2a6dc941a74fbb3e0ea80e78a8876b
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-02-06 02:37:37 -0800

    major fixes

--- src/components/ui/animated-testimonials.tsx
+++ src/components/ui/animated-testimonials.tsx
@@ -83,7 +83,7 @@ export const AnimatedTestimonials = ({
                     width={400}
                     height={400}
                     draggable={false}
-                    className="h-full w-full rounded-3xl object-cover object-center md:w-[500px] w-[300px]"
+                    className="h-full rounded-3xl object-cover object-center md:w-[500px] w-[300px]"
                   />
                 </motion.div>
               ))}
