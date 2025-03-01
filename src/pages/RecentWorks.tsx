79447fbfd5c519c46ad7756b618be037bfc67bd2
Caleb Kalejaiye <155694747+heycalebszn@users.noreply.github.com>
2025-03-01 01:56:22 +0100

    Update RecentWorks.tsx

--- src/pages/RecentWorks.tsx
+++ src/pages/RecentWorks.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { ArrowUpRight, Github } from "lucide-react";
 import { Afrovivo, Baba, Ecoride, MockTrade, MultiTrading, Portscan, Rewrap, Stacist } from "../assets";
 
@@ -128,11 +127,8 @@ const RecentWorks = () => {
             </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project, index) => (
-            <motion.div
+            <div
               key={index}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
               className="bg-[#0a1c20] rounded-2xl p-6 backdrop-blur-sm justify-between flex flex-col"
             >
               <img
@@ -170,7 +166,7 @@ const RecentWorks = () => {
                   <Github />
                 </a>
               </div>
-            </motion.div>
+            </div>
           ))}
         </div>
       </div>
