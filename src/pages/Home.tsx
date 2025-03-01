992adf667886df07ea6c5550b1a8e27ac6315dac
Caleb Kalejaiye <155694747+heycalebszn@users.noreply.github.com>
2025-03-01 03:06:48 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -52,8 +52,8 @@ const Hero = () => {
                 className={`h-auto w-auto`}
               >
                 <TwIcon
-                  fill={`#6f1cd7`}
-                  className={`w-[30px] h-[30px] rounded-full border-[#0a1c20] border-solid border-[2px] p-1  transition-all duration-500 hover:bg-white`}
+                  fill={`#FFCC00`}
+                  className={`w-[30px] h-[30px] rounded-full border-[#FFCC00] border-solid border-[2px] p-1  transition-all duration-500 hover:bg-white`}
                 />
               </motion.a>
               <motion.a
