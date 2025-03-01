ca0a80ec3872b3f7d10f79f7013d7a58ad666f8c
Caleb Kalejaiye <155694747+heycalebszn@users.noreply.github.com>
2025-03-01 09:30:58 +0100

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -9,7 +9,7 @@ export const projects = [
 {
     name: "Xdegen",
     desc: "A sefer way to trade degen/meme coins.",
-    lang: "Next.js + Node.js + Express.js + MongoDB",
+    lang: "Next.js + ReactJS + TaiwimdCSS + Typeacript",
     link: "https://waitlist.xdegen.xyz/",
     github: "https://github.com/heycalebszn",
   },
